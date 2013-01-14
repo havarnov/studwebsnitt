64a680eea7d5fb3a9b0d421b25d069e995f41ec3
Håvar Nøvik <havar.novik@gmail.com>
2013-01-14 12:10:50 +0100

    BUG: needs to be bound to the right port on heroku

--- studwebsnitt/studwebsnitt.py
+++ studwebsnitt/studwebsnitt.py
@@ -93,4 +93,5 @@ def index():
 if __name__ == "__main__":
     app.secret_key = SECRET
     app.debug = True
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
