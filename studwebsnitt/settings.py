d09add528d5e66dbf7e775a552ba91037ce0acba
Håvar Nøvik <havar.novik@gmail.com>
2013-01-14 12:10:14 +0100

    using os.environ.get insted of a try...except clause

--- studwebsnitt/settings.py
+++ studwebsnitt/settings.py
@@ -1,9 +1,7 @@
 import os
 
-try:
-	SECRET = os.environ["APP_SECRET"]
-except KeyError:
-	SECRET = None
+
+SECRET = os.environ.get("APP_SECRET", None)
 
 dir = os.path.dirname(__file__)
 if os.path.exists(os.path.join(dir, "local_settings.py")):
